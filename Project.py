2fe88f7f66caf4b4752d436e888ae96356a3902b
300FR <60340994+300FR@users.noreply.github.com>
2020-11-10 11:02:14 +0100

    MMXQM

--- Project.py
+++ Project.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 df=pd.read_csv("./BDPV-opendata-installations/BDPV-opendata-installations.csv", error_bad_lines=False)
 type(df)
