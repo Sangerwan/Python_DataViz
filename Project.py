5686aa1b1182a10bebed456df1fdb6b2535c60f6
Sangerwan <sangerwan@gmail.com>
2020-11-20 11:04:43 +0100

    helo

--- Project.py
+++ Project.py
@@ -7,9 +7,29 @@ import os
 #https://www.data.gouv.fr/fr/datasets/donnees-sur-les-installations-photovoltaique-en-france-et-quelques-pays-europeens/
 
 path = os.getcwd()
-
+####
+#Pas_dans la liste
+#Sud
+#production 0
+#orientation_optimum nan
+#pente optimum
+#postal_code_suffix
+###
 print(path)
 df=pd.read_csv("./BDPV-opendata-installations/BDPV-opendata-installations.csv", sep=';')
+#Remove sud
+df['orientation']=df['orientation'].replace("Sud",0);# SUD=0
+df['orientation']=df['orientation'].replace("South",0);# SUD=0
+df['orientation']=df['orientation'].replace("Sur",0);# SUD=0
+#remove Pas_dans_la_liste_panneaux
+df=df[df.panneaux_modele != 'Pas_dans_la_liste_panneaux']
+#remove 0 prod
+df=df[df.production_pvgis != 0]
+
+
+for col in df:
+    print(col)
+    print(df[col].unique())
 
 print('describe')
 print(df.describe())
